c308260a8fff23a802f7b9b8f02f97677f0dd147
mlesigues <marielle_lesigues029@yahoo.com>
2018-12-02 13:37:04 -0800

    final login file

--- login.php
+++ login.php
@@ -1,17 +1,17 @@
 <?php
-include("config.php");
 
 // Initialize the session
 session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: index.php");
     exit;
 }
  
 // Include config file
 //require_once ("config.php");
+include("config.php");
  
 // Define variables and initialize with empty values
 $username = $password = "";
@@ -66,7 +66,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: index.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
